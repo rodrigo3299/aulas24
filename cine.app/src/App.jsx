03a64607f645a749991a9ad2542c025c6ec7f0d4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 13:01:30 -0300

    aula

---
+++ cine.app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./componentes/Header/Header"
+import {Outlet } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ cine.app/src/componentes/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar"
+
+function Header() {
+    return ( 
+        <header>
+            <span>Olá, visitante!</span>
+            <h1>Bem-vindo ao CineApp!</h1>
+            <Navbar/>
+        </header>
+     );
+}
+
+export default Header;
