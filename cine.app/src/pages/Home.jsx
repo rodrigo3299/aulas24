ac301e934aabf5cd146e453418d52a6c8897bed4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 12:54:10 -0300

    aula

--- cine.app/src/pages/Home.jsx
+++ cine.app/src/pages/Home.jsx
@@ -3,18 +3,31 @@ import data from '../../artigos.json'
 function Home() {
   console.log(data)
     return (  
-        <main>
-        <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca"/>
-         <div className='lista-filmes'>
-           {data.map(
-            filme => (
-              <h1>{filme.title}</h1>
-            )
-           )
+        <>
+        <input type="text" name="busca" id="busca" placeholder='Digite aqui sua busca'/>
+         <div className='grid grid-cols-3'>
+
+           {data.map(filme => (
+              <div className='card' key={filme.title}>
+                <h1>{filme.title}</h1>
+                <img src={filme.image} alt={filme.title}/>
+                <div className='tag bg-black '>
+                 {filme.tags.map(tag =>(
+                  <span key={tag}className='text-red-900'> {tag}</span>
+                 ))}
+              </div>
+              <div className='text'>
+                 {filme.text.map(texto =>(
+                  <p key={texto}> {texto}</p>
+                 ))}
+              </div>
+          </div>
+
+            ))
           }
-         </div>
-        </main>
+          </div>
+      </>
     );
 }
 
-export default Home;
+export default Home ;
