6ee45b87795517faa44061cbdfc2f96305a55d4c
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2024-06-10 12:14:34 -0300

    Update Home.jsx

--- cine.app/src/pages/Home.jsx
+++ cine.app/src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
               <div className='card' key={filme.title}>
                 <h1>{filme.title}</h1>
                 <img src={filme.image} alt={filme.title}/>
-                <div className=' '>
+                <div className='tag'>
                  {filme.tags.map(tag =>(
                   <span key={tag}className='text-red-900'> {tag}</span>
                  ))}
