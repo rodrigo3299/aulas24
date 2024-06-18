ab345b2902a4af3cb8258eaa95470d8514062d34
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-18 11:46:42 -0300

    18/6

--- cine.app/src/pages/Filmes.jsx
+++ cine.app/src/pages/Filmes.jsx
@@ -1,31 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import {Link} from 'react-router-dom'
 
 function Filmes() {
 
-    const [filmes, setFilmes] = useState([])
+    const [filmes, setFilme] = useState([])
 
     const apiKey= '7c572a9f5b3ba776080330d23bb76e1e'
     const urlBase ='https://api.themoviedb.org/3/movie/' 
     const urlImg = 'https://image.tmdb.org/t/p/w342/'
 
-    fetch(`${urlBase}popular?api_key=${apiKey}`)
-    .then(response => response.json())
-    .then(response => setFilmes(response.results))
-    .catch(erro => console.log(erro))
+    useEffect( () => {
+        fetch(`${urlBase}popular?${apiKey}`)
+        .then(response => response.json())
+        .then(response => setFilme(response.results))
+        .catch(erro => console.log(erro))
+    },[])
     return ( 
     <>
-        <h1>Filmes</h1>
+        
+        <div className="listaFilmes grid grid-cols-3">
         {
             filmes.map(filme => (
                 <div className="card-filme" key={filme.id}>
-                <h1>{filme.title}</h1>
                     <img src={`${urlImg}${filme.poster_path}`}/>
-                    <p>{filme.overview}</p>
-                    </div>
+                    <h1>{filme.title}</h1>
+                    <Link to={`${filme.id}`} className="bg-blue-500">Saber mais</Link>
+                </div>
             ))
         }
-    
-    </> );
+    </div>
+    </> 
+    );
 }
 
 export default Filmes;
---
+++ cine.app/src/pages/PaginaFilme.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom'
+
+
+function PaginaFilme(){
+
+    const {id} = useParams();
+
+    const [filme, setFilme] = useState([])
+
+    const apiKey='api_key=d1bef122d48992b960cc79c7306f5c1b'
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const urlImg = 'https://image.tmdb.org/t/p/w1280/'
+
+    useEffect( () => {
+        fetch(`${urlBase}${id}?${apiKey}&language=pt-BR`)
+        .then(response => response.json())
+        .then(response => setFilme(response))
+        .catch(erro => console.log(erro))
+    },[])
+
+    return(
+        <>
+            <img src= {`${urlImg}${filme.backdrop_path}`} />
+            <h1>{filme.title}</h1>
+            <p>{filme.overview}</p>
+        </>
+        
+
+    )
+}
+
+export default PaginaFilme
