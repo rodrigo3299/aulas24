e3c048f11906273c0393d9ad853156f839280845
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 10:04:56 -0300

    revisao

---
+++ revisao/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [contador, setContador] = useState(0)
+
+function handleClick() {
+  setContador((anterior)=> (
+    anterior + 1
+  ))
+  console.log(contador)
+}
+
+  return (
+    <>
+     <p>{contador}</p>
+     <button onClick={handleClick}> Aumentar </button>
+    </>
+  )
+}
+
+export default App
