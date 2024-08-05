1261fa0677ee0a88b7cbdebaf7469f208bdd0074
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2024-08-05 11:17:31 -0300

    Update Contador.jsx

--- revisao/src/Contador.jsx
+++ revisao/src/Contador.jsx
@@ -1,1 +1,24 @@
 
+import { useState } from 'react';
+
+export default function Contador() {
+  const [contador, setContador] = useState(0)
+
+function handleClick() {
+  setContador((anterior)=> (
+    anterior + 1
+  ))
+  console.log(contador)
+}
+
+  return (
+    <>
+     <p>{contador}</p>
+     <button onClick={handleClick}> Aumentar </button>
+    </>
+    
+  )
+}
+
+
+
