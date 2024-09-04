d912df01ce5cf4c644b6f90b2f52b88fce9ac90b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:17:07 -0300

    aula

---
+++ aula/spoty/src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+ 
+ 
+  return (
+    <>
+      <div className="bg-pink-100">
+        <nav className='bg-pink-700 bg-cover w-full h-28 '></nav>
+        <div className="bg-blue-600 bg-cover w-48 h-screen p-10">
+          <div className="bg-pink-700 bg-cover w-28 h-28  flex justify-center items-center mb-10">
+ 
+          </div>
+ 
+          <div className="bg-pink-700 bg-cover w-28 h-28 flex justify-center items-center mb-10 ">
+ 
+          </div>
+ 
+          <div className="bg-pink-700 bg-cover w-28 h-28  flex justify-center items-center ">
+ 
+          </div>
+          
+        </div>
+      </div>
+ 
+ 
+    </>
+  )
+}
+ 
+export default App
---
+++ aula/spoty/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-digolinha': '#F41B1B'
+      }
+    },
+  },
+  plugins: [],
+}
